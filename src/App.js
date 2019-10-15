ce1150c894f2a6698a4b6a8e66ed4a9feb95e553
Juan Aleman <juan.j.aleman@gmail.com>
2019-10-14 19:35:42 -0500

    added search bar and bck-grnd img

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
 import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
 
 export default function App() {
   return (
@@ -11,12 +12,12 @@ export default function App() {
         <Header />
         <Link to="/">Home</Link>
         <Link to="/character-list">Character List</Link>
+        <SearchForm />
       </nav>
 
       <Route exact path="/" component={WelcomePage}/>
       <Route path="/character-list" component={CharacterList}/>
 
-      {/* <CharacterList /> */}
     </main>
   );
 }
